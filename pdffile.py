1564c5554c7b9491cdd3b6d7789d67cbce454397
sandeepcodeops <sandeep@codeops.tech>
2021-12-04 20:10:18 +0530

    new pdf file

--- pdffile.py
+++ pdffile.py
@@ -1,4 +1,4 @@
 import pdfkit
 
 config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
-pdfkit.from_string('crime.html', 'crimePDF.pdf', configuration=config)
+pdfkit.from_file('crime.html', 'crimePDF.pdf', configuration=config)
