74801a62913e16cc430fed4523f3c34b15ae9bf5
sandeepcodeops <sandeep@codeops.tech>
2021-12-06 13:13:53 +0530

    a

--- pdffile.py
+++ pdffile.py
@@ -2,4 +2,3 @@ import pdfkit
 
 config = pdfkit.configuration(wkhtmltopdf="C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe")
 pdfkit.from_file('crime.html', 'crimePDF.pdf', configuration=config)
-2
